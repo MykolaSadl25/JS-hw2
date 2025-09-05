639a5d725a5918a1a1dd3fa9c099ae209b0bee95
Mykola Sadlii <mykolac08@gmail.com>
2025-09-05 19:18:45 +0100

    part 1

---
+++ js/numbers.js
@@ -0,0 +1,50 @@
+// Task 1
+const celsius = 21;
+const fahrenheit = (celsius * 9/5) + 32;
+console.log(fahrenheit);
+
+// Task 2
+const daysInMonth = 30;
+const hoursInMonth = daysInMonth * 24;
+const minutesInMonth = daysInMonth * 24 * 60;
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+
+// Task 3
+const health = 100;
+const energy = 100;
+const decreasedHealth = health - 34;
+const decreasedEnergy = energy - 64;
+console.log(decreasedHealth);
+console.log(decreasedEnergy);
+
+// Task 4
+const totalPrice = 100;
+const discount = 100 - (totalPrice * 0.10)
+const discountedPrice = discount
+console.log(discountedPrice);
+
+// Task 5
+
+// Task 6
+const floatString = "45.67";
+const parsedFloat = parseFloat(floatString);
+console.log(parsedFloat);
+
+// Task 7
+
+const intString = "123";
+const parsedInt = parseInt(intString);
+console.log(parsedInt);
+
+// Task 8
+
+// Task 9
+
+const integer = 42;
+const stringNumber = "256";
+const convertedInt = parseInt(stringNumber);
+console.log(convertedInt);
+const convertedString = parseFloat(integer);
+console.log(convertedString);
+
